3978f4bf4c9f89d693a5ed973e1cc3215ac3655e
Abderazzak Oumad <aoumad@e2r8p7.1337.ma>
2023-12-03 20:41:40 +0100

    done

--- mytasks/src/app/components/statistics/Global_Rating/Global_Rating.tsx
+++ mytasks/src/app/components/statistics/Global_Rating/Global_Rating.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Global_Rating.module.css'
 import Global_RatingList from './Global_RatingList';
+import { GiSettingsKnobs } from "react-icons/gi";
 
 interface Rating {
     id: string;
@@ -19,6 +20,7 @@ const Global_Rating: React.FC<RatingListProps> = ({ratings}) => {
     <div className={styles['global-rating']}>
         <div className={styles['global-rating-title']}>
             <span>Global Rating</span>
+            <GiSettingsKnobs className={styles['global-rating-icon']}/>
         </div>
         <Global_RatingList ratings={ratings} />
     </div>
